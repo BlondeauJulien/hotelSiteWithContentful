15605584e2fd11c7c8305b404303e33f78f108bd
Julien Blondeau <julienblondeau@live.fr>
2021-03-05 16:10:20 +0100

    create hero banner

---
+++ src/components/Hero.js
@@ -0,0 +1,14 @@
+
+const Hero = ({ children, hero }) => {
+  return (
+    <header className={hero}>
+      { children }
+    </header>
+  )
+}
+
+export default Hero;
+
+Hero.defaultProps = {
+  hero: 'defaultHero'
+}
--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+import Hero from '../components/Hero';
+import Banner from '../components/Banner';
 
 const Error = () => {
   return (
-    <div>
-      Hello from error
-    </div>
+    <Hero>
+      <Banner title="404" subtitle="page not found">
+        <Link to="/" className="btn-primary">
+          return home
+        </Link>
+      </Banner>
+    </Hero>
   )
 }
 
